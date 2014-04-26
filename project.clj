a36232dfc9fd596dc27ffa7287d0d06db700dbff
William Roe <willroe@gmail.com>
2014-04-26 15:19:30 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject melody-lake "0.1.0-SNAPSHOT"
+  :description "Ludum Dare 29 entry - Melody Lake"
+  :url "http://www.ludumdare.com/compo/2014/04/23/welcome-to-ludum-dare-29er/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2202"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-simpleton "1.3.0"]]
+  :source-paths ["src"]
+  :cljsbuild {:builds
+              {:dev
+               {:source-paths ["src"]
+                :compiler {:output-dir "resources/public"
+                           :output-to "resources/public/game.js"
+                           :optimizations :whitespace
+                           :pretty-print true
+                           :source-map "resources/public/game.js.map"}}
+               :prod
+               {:source-paths ["src"]
+                :compiler {:output-to "resources/public/game.adv.js"
+                           :pretty-print false
+                           :optimizations :advanced}}}})
---
+++ src/melody_lake/core.cljs
@@ -0,0 +1,11 @@
+(ns melody-lake.core
+  (:require [goog.dom :as dom]
+            [goog.style :as style]
+            [goog.Timer]
+            [goog.events :as events]
+            [goog.events.KeyCodes :as kcs]
+            [goog.events.EventType :as event-type]))
+
+
+
+
