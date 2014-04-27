d1e0fe22d7bfc35f813074255ed88a55b882e5e9
William Roe <willroe@gmail.com>
2014-04-27 17:23:25 +0100

    core.async

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]]
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-simpleton "1.3.0"]]
   :source-paths ["src"]
--- src/melody_lake/core.cljs
+++ src/melody_lake/core.cljs
@@ -1,11 +1,11 @@
 (ns melody-lake.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [goog.dom :as dom]
             [goog.style :as style]
             [goog.Timer]
             [goog.events :as events]
             [goog.events.KeyCodes :as kcs]
-            [goog.events.EventType :as event-type]))
-
-
+            [goog.events.EventType :as event-type]
+            [cljs.core.async :refer [put! chan <!]]))
 
 
